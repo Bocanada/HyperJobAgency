6e6b54492d1e5d000702ace4911ed426e3b83648
Wiped Out <mauromathers@icloud.com>
2020-06-09 02:26:20 -0300

    qasd

--- HyperJob Agency/hyperjob/vacancy/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from vacancy.models import Vacancy
-from django.views import View
-# Create your views here.
-
-class VacancyPage(View):
-
-    def get(self, request, *args, **kwargs):
-        return render(request, 'vacancy/vacancies.html', context={'vacancies': Vacancy.objects.all()})
